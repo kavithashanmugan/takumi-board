ac3a55ac2d4f27966191f062f56262b1a84be04f
Kavitha Shanmugan <kavitha.shanmugan@gmail.com>
2025-01-02 09:59:27 -0600

    fix:styling issues

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <Navbar />
-    <div className="mt-[10%] text-center">
+    <div className="my-[10%] text-center">
     <Board />
     </div>
   </div>
